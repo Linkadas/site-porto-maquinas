2811c48aac0e6198c6729d8fff0f9f37d94745c7
Lincoln Asaga <lincolnshigueo@hotmail.com>
2026-07-19 19:36:18 -0300

    chore: alterar fundo para rosa claro conforme solicitado

--- force_cache.py
+++ force_cache.py
@@ -8,12 +8,12 @@ for file in html_files:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
 
-    # Append ?v=6 to any .css that does not already have a query parameter
-    # Example: href="./css/index.css" -> href="./css/index.css?v=6"
-    new_content = re.sub(r'\.css(["\'])', r'.css?v=6\1', content)
+    # Append ?v=7 to any .css that does not already have a query parameter
+    # Example: href="./css/index.css" -> href="./css/index.css?v=7"
+    new_content = re.sub(r'\.css(["\'])', r'.css?v=7\1', content)
     
-    # Also bump existing ?v=X to ?v=6
-    new_content = re.sub(r'\.css\?v=\d+(["\'])', r'.css?v=6\1', new_content)
+    # Also bump existing ?v=X to ?v=7
+    new_content = re.sub(r'\.css\?v=\d+(["\'])', r'.css?v=7\1', new_content)
 
     if new_content != content:
         with open(file, 'w', encoding='utf-8') as f:
